460361655f536d40431b0e0966c34bf5d8998dbd
hira <hirapoke823@gmail.com>
2020-03-18 07:31:04 +0900

    project新規作成

---
+++ FoodMemo/FoodMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodMemo
+//
+//  Created by 平島峻成 on 2020/03/18.
+//  Copyright © 2020 平島峻成. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
