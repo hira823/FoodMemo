6f373a341449b41a9202a50970a10efc1d19f5b4
hira <hirapoke823@gmail.com>
2020-03-19 07:53:35 +0900

    食材残量記録画面の作成

---
+++ FoodMemo/FoodMemo/AFR/AFR01ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AFR01ViewController.swift
+//  FoodMemo
+//
+//  Created by 平島峻成 on 2020/03/19.
+//  Copyright © 2020 平島峻成. All rights reserved.
+//
+
+import UIKit
+
+class AFR01ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
